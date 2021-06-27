6ff8ae75bacb25a154aec481322d4cc05e3b8d03
Lucienne <kinako890419@gmail.com>
2021-06-27 13:07:32 +0800

    Initial Commit

---
+++ MemoryGameDemo/MemoryGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameDemoApp.swift
+//  MemoryGameDemo
+//
+//  Created by 兔子 on 2021/6/27.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
